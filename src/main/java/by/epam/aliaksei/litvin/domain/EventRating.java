a162d93e48605cad8d0d06e447f925c4c935ee79
AlekseiLitvin <voodoochileb@gmail.com>
2017-10-03 22:29:46 +0300

    initial commit

---
+++ src/main/java/by/epam/aliaksei/litvin/domain/EventRating.java
@@ -0,0 +1,14 @@
+package by.epam.aliaksei.litvin.domain;
+
+/**
+ * @author Yuriy_Tkach
+ */
+public enum EventRating {
+
+    LOW,
+
+    MID,
+
+    HIGH;
+
+}
---
+++ src/main/java/by/epam/aliaksei/litvin/service/UserService.java
@@ -0,0 +1,24 @@
+package by.epam.aliaksei.litvin.service;
+
+
+
+import by.epam.aliaksei.litvin.domain.User;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * @author Yuriy_Tkach
+ */
+public interface UserService extends AbstractDomainObjectService<User> {
+
+    /**
+     * Finding user by email
+     * 
+     * @param email
+     *            Email of the user
+     * @return found user or <code>null</code>
+     */
+    public @Nullable User getUserByEmail(@Nonnull String email);
+
+}
